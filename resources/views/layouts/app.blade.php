b6637a447654e3e4e9694b05f74c6058b7ac184d
unknown <naila29rosa@gmail.com>
2021-12-15 23:25:35 +0700

    Perbaikan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,13 +34,13 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500&display=swap">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/jquery.mCustomScrollbar.min.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/media-queries.css">
+	<link rel="stylesheet" href="{{asset('assets/css/jquery.mCustomScrollbar.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/media-queries.css')}}">
 
     <!-- Icon -->
-    <link rel="shortcut icon" href="assets/img/logo-icon.png">
+	<link rel="stylesheet" href="{{asset('assets/img/logo-icon.png')}}">
 
 	
 	</head>
