c608d51d90c2544199b38529b4b019d45fcfec17
unknown <naila29rosa@gmail.com>
2021-12-15 22:02:34 +0700

    Sidebar Menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,20 +1,54 @@
-<div id="colorlib-page">
-		<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
-		<aside id="colorlib-aside" role="complementary" class="border js-fullheight">
-			<h1 id="colorlib-logo"><a href="brand">Parfume</a></h1>
-			<nav id="colorlib-main-menu" role="navigation">
-				<ul>
-					<li class="colorlib-{{ ($active === "brand") ? 'active' : '' }}"><a href="brand">Brand</a></li>
-					<li class="colorlib-{{ ($active === "product") ? 'active' : '' }}"><a href="product">Product</a></li>
-					<li class="colorlib-{{ ($active === "acara") ? 'active' : '' }}"><a href="acara">Event</a></li>
-				</ul>
-			</nav>
+<!-- Wrapper -->
+<div class="wrapper">
 
-			<div class="colorlib-footer">
-				<p><small>&copy; Kelompok 2
-			</div>
+	<!-- Sidebar -->
+	<nav class="sidebar">
+		
+		<!-- close sidebar menu -->
+		<div class="dismiss">
+			<i class="fas fa-arrow-left"></i>
+		</div>
+		
+		<div class="logo">
+			<h3><a href="brand">Parfume</a></h3>
+		</div>
+		<ul class="list-unstyled menu-elements">
+			<li class="active">
+				<a class="scroll-link-{{ ($active === "brand") ? 'active' : '' }}" href="brand">Brand</a>
+			</li>
+			<li>
+				<a class="scroll-link-{{ ($active === "product") ? 'active' : '' }}" href="product">Product</a>
+			</li>
+			<li>
+				<a class="scroll-link-{{ ($active === "acara") ? 'active' : '' }}" href="acara">Event</a>
+			</li>
+		</ul>
+		
+		<div class="to-top">
+			<a class="btn btn-primary btn-customized-3" href="#" role="button">
+				<i class="fas fa-arrow-up"></i> Top
+			</a>
+		</div>
+		
+		<div class="dark-light-buttons">
+			<a class="btn btn-primary btn-customized-4 btn-customized-dark" href="#" role="button">Dark</a>
+			<a class="btn btn-primary btn-customized-4 btn-customized-light" href="#" role="button">Light</a>
+		</div>
+	
+	</nav>
+	<!-- End sidebar -->
+	
+	<!-- Dark overlay -->
+	<div class="overlay"></div>
 
-		</aside>
+	<!-- Content -->
+	<div class="content">
+	
+		<!-- open sidebar menu -->
+		<a class="btn btn-primary btn-customized open-menu" href="#" role="button">
+			<i class="fas fa-align-left"></i> <span>Menu</span>
+		</a>
 
 		@yield('content')
 	</div>
+</div>
