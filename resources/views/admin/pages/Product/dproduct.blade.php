e8aa7604a8bec205762a179dd39855c063d78f9e
baharmaulidi <66855624+baharmaulidi@users.noreply.github.com>
2021-12-15 16:43:09 +0700

    Update dproduct.blade.php

--- resources/views/admin/pages/Product/dproduct.blade.php
+++ resources/views/admin/pages/Product/dproduct.blade.php
@@ -4,34 +4,34 @@
     <div class="col-md-20">
         <div class="card">
             <div class="card-body">
-                <h4 class="card-title">Detail Event</h4>
+                <h4 class="card-title">Detail Product</h4>
                 <div class="form-group row">
-                    <div class="col-md-9">
-                        @if ($data->brand_image != '')
-                            <img src="{{ asset('admin_assets/images/Products/' . $data->product_image) }}" alt="Gambar Event"
-                                style="display:block; margin:auto;" width="200px" height="175px">
+                    <div class="col-md-20">
+                        @if ($data->product_image != '')
+                            <img src="{{ asset('admin_assets/images/Products/' . $data->product_image) }}" alt="Gambar Product"
+                            style="display:block; margin:auto;" width="200px" height="175px">
                         @else
-                            <img src="{{ asset('admin_assets/images/Products/imgproductdefault.png') }}" alt="Gambar Event" width="200px"
-                                style="display:block; margin:auto;" height="175px">
+                            <img src="{{ asset('admin_assets/images/Products/imgproductdefault.png') }}" alt="Gambar Product" width="200px"
+                            style="display:block; margin:auto;" height="175px">
                         @endif
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="dproductname" class="col-sm-3 text-end control-label col-form-label">Product Name</label>
+                    <label for="dproductname" class="col-sm-2 text-end control-label col-form-label">Product Name</label>
                     <div class="col-sm-9">
                         <input type="text" class="form-control" value="{{ $data->product_name }}" disabled >
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="desc" class="col-sm-3 text-end control-label col-form-label">Description</label>
+                    <label for="desc" class="col-sm-2 text-end control-label col-form-label">Description</label>
                     <div class="col-sm-9">
                         <textarea class="form-control" style="margin-top: 0px; margin-bottom: 0px; height: 50px;" disabled >
-                            {{ $data->product_desc }}
+                        {{ $data->product_desc }}
                         </textarea>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="dprice" class="col-sm-3 text-end control-label col-form-label">Price</label>
+                    <label for="dprice" class="col-sm-2 text-end control-label col-form-label">Price</label>
                     <div class="col-sm-9">
                         <input type="text" class="form-control" value="{{ $data->product_price }}" disabled >
                     </div>
